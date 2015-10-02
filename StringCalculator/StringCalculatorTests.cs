e7d339545226eca8081e3bc0767f1140e6c167bd
Monomachus <monomachus85@yahoo.com>
2015-10-02 16:57:59 +0300

    First commit. First test

---
+++ StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class StringCalculatorTests
+    {
+        [Test]
+        public void Add_WhenEmptyString_ReturnsZero()
+        {
+            StringCalculator stringCalculator = new StringCalculator();
+
+            var result = stringCalculator.Add("");
+            Assert.AreEqual(result, 0);
+        }
+
+
+    }
+}
